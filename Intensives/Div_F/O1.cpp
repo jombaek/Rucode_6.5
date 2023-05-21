cf63846e64c81b3c96baa86273954a027eb6f363
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-05-21 20:58:31 +0700

    Div F O1. Refereeing

--- Intensives/Div_F/O1.cpp
+++ Intensives/Div_F/O1.cpp
@@ -2,26 +2,32 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip> 
+#include <limits>
 
 int main()
 {
-    int n = 5;
+    int n = 5, sum = 0;
+	int minScore = std::numeric_limits<int>::max();
+	int maxScore = std::numeric_limits<int>::min();
     std::vector<int> v(n);
 
     for (auto i = 0; i < n; ++i)
+    {
         std::cin >> v[i];
+    
+    	if (v[i] > maxScore)
+        	maxScore = v[i];
+    
+    	if (v[i] < minScore)
+        	minScore = v[i];
+    	
+    	sum += v[i];
+    }
 
-    std::sort(v.begin(), v.end());
-
-    std::cout << v[0] << " " << v[n - 1] << '\n';
-
-    int sum = 0;
-    for (auto i = 1; i < n - 1; ++i)
-        sum += v[i];
-
+    std::cout << minScore << " " << maxScore << '\n';
     std::cout << std::fixed;
     std::cout << std::setprecision(2);
-    std::cout << sum / 3.0;
+    std::cout << (sum - minScore - maxScore) / 3.0;
 
     return 0;
 }
